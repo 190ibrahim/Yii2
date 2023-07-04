e86b68d9009a3b4084f350df2f3d088b9da591e8
190ibrahim <190ibrahimahmed@gmail.com>
2023-07-04 17:33:15 +0200

    Important concepts

---
+++ views/site/privacy-policy.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'Privacy Policy';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-privacy-policy">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is the privacy-policy page. You may modify the following file to customize its content:
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
